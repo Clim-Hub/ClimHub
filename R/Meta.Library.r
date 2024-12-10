824783dad13103f0661ee6e2726e2761a0f206d5
ErikKusch <erik@i-solution.de>
2024-12-10 10:38:37 +0100

    Meta.Library now using webscraper instead of API

--- R/Meta.Library.r
+++ R/Meta.Library.r
@@ -4,7 +4,10 @@
 #' Provide an overview of all data sets for which metadata files are present.
 #'
 #' @importFrom tools file_path_sans_ext
-#' @importFrom supportR github_ls
+#' @importFrom rvest read_html
+#' @importFrom rvest html_nodes
+#' @importFrom rvest html_text
+#' @importFrom jsonlite fromJSON
 #'
 #' @return A vector of supported datasets.
 #'
@@ -20,9 +23,25 @@ Meta.Library <- function() {
     path <- "product-metadata"
 
     # Construct the URL
-    URL <- paste0("https://github.com/", owner, "/", repo)
+    URL <- paste0("https://github.com/", owner, "/", repo, "/tree/master/", path)
 
-    # list files
-    file_names <- supportR::github_ls(URL, folder = path, quiet = TRUE)$name
-    tools::file_path_sans_ext(file_names)
+    # Read the page
+    page <- rvest::read_html(URL)
+
+    # Extract the embedded JSON metadata
+    raw_metadata <- page %>%
+        rvest::html_nodes("script") %>% # Extract all <script> tags
+        rvest::html_text() %>% # Get the text content
+        grep("payload", ., value = TRUE) %>% # Locate the script containing "payload"
+        jsonlite::fromJSON(simplifyVector = FALSE) # Parse JSON metadata
+
+    # Navigate to the relevant "tree" section
+    tree_items <- raw_metadata$payload$tree$items
+
+    # Filter for JSON files
+    tools::file_path_sans_ext(unlist(lapply(tree_items, "[[", "name")))
 }
+
+
+
+library(rvest)

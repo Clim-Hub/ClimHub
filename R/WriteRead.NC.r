c1eb2a710324762769a0e967ebf95fdcdea72d22
ErikKusch <erik@i-solution.de>
2025-05-21 16:09:36 +0200

    WriteRead.NC now loads all attributes in NC when loading thus simplifying the loading process

--- R/WriteRead.NC.r
+++ R/WriteRead.NC.r
@@ -5,12 +5,12 @@
 #'
 #' @param NC SpatRaster
 #' @param FName Filename including directory
-#' @param Variable Character. Variable name to be saved as varname in NC output.
-#' @param LongVar Character. Character. Long variable name.
-#' @param Unit Character. Unit in which Variable is recorded.
-#' @param Attrs Named vector of metadata attributes
 #' @param Write Logical. Whether to write metadata
-#' @param Compression Integer between 1 to 9. Applied to final .nc file that the function writes to hard drive. Same as compression argument in terra::writeCDF().
+#' @param Variable Character. Needed for writing. Variable name to be saved as varname in NC output.
+#' @param LongVar Character. Needed for writing. Long variable name.
+#' @param Unit Character. Needed for writing. Unit in which Variable is recorded.
+#' @param Attrs Named vector of metadata attributes to be written. Needed for writing.  When reading, all attributes in the .nc file are read.
+#' @param Compression Integer between 1 to 9. Needed for writing. Applied to final .nc file that the function writes to hard drive. Same as compression argument in terra::writeCDF().
 #'
 #' @importFrom terra writeCDF
 #' @importFrom ncdf4 nc_open
@@ -21,7 +21,7 @@
 #'
 #' @return A SpatRaster with metadata
 #'
-WriteRead.NC <- function(NC, FName, Variable, LongVar, Unit = "NA", Attrs, Write = FALSE, Compression = 1) {
+WriteRead.NC <- function(NC, FName, Variable, LongVar, Unit = "NA", Attrs = NULL, Write = FALSE, Compression = 1) {
   ## remove = signs in variable vector, these break metags assignment
   Attrs <- gsub(pattern = "=", replacement = "...", Attrs)
 
@@ -35,24 +35,20 @@ WriteRead.NC <- function(NC, FName, Variable, LongVar, Unit = "NA", Attrs, Write
       compression = Compression,
       attrs = Attrs
     )
-    NC <- rast(FName)
-    # NC <- writeCDF(x = NC, filename = FName, varname = Variable, unit = Unit, longname = longnames(NC), compression = Compression)
-    # nc <- nc_open(FName, write = TRUE)
-    # for (name in names(Attrs)) {
-    #   ncatt_put(nc, varid = 0, attname = name, attval = Attrs[[name]])
-    # }
-    # nc_close(nc)
   }
-  ## Reading metadata
+  ## we need to load NC now
+  NC <- rast(FName)
+  ## Reading metadata and assigning it to returned raster
   nc <- nc_open(FName)
-  # Retrieve custom metadata
-  Meta <- lapply(names(Attrs), FUN = function(name) {
-    ncatt_get(nc, 0, name)$value
-  })
-  # Close the NetCDF file
+  Meta <- ncdf4::ncatt_get(nc, 0)
+  # # Retrieve custom metadata
+  # Meta <- lapply(names(Attrs), FUN = function(name) {
+  #   ncatt_get(nc, 0, name)$value
+  # })
+  # # Close the NetCDF file
   nc_close(nc)
   Meta_vec <- unlist(Meta)
-  names(Meta_vec) <- names(Attrs)
+  # names(Meta_vec) <- names(Attrs)
   terra::metags(NC) <- Meta_vec
 
   ## return object

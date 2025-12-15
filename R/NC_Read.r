d308aba2dd4bbde6f46dbdd98a20abeed2f72c70
ErikKusch <erik@i-solution.de>
2025-12-15 11:44:14 +0100

    First attempt at asTerra flag in NC_Read

--- R/NC_Read.r
+++ R/NC_Read.r
@@ -1,16 +1,32 @@
 #' @title Read a netCDF file
 #'
-#' @description Open and read a netCDF file including its metadata. Data is
-#'   returned as a `ncdfCF` object.
+#' @description Open and read a netCDF file including its metadata.
 #'
-#' @param resource Character. Fully qualified file name for a netCDF file on a
-#'   local file system or the URL to a netCDF resource on a THREDDS server.
+#' @param fileName Character. Fully qualified file name for a netCDF file on a local file system or the URL to a netCDF resource on a THREDDS server.
+#' @param asTerra Logical. If `TRUE` (default), the contents of the netCDF file is returned as a `SpatRaster` object from the `terra` package.
 #'
-#' @return A `ncdfCF` data set instance.
-#' @author Patrick Van Laake
+#' @return Either a `SpatRaster` object or an `ncdfCF` data set instance.
+#'
+#' @author Patrick Van Laake, Erik Kusch
+#'
+#' @importFrom ncdfCF open_ncdf
+#'
 #' @examples
-#' ds <- NC_Read(resource = system.file("extdata", "KiN_rast.nc", package = "ClimHub"))
+#' ## loading as ncdfCF object
+#' NC_Read(fileName = system.file("extdata", "KiN_rast.nc", package = "ClimHub"), asTerra = FALSE)
+#' ## loading as terra SpatRaster object
+#' NC_Read(fileName = system.file("extdata", "KiN_rast.nc", package = "ClimHub"))
 #' @export
-NC_Read <- function(resource) {
-  ncdfCF::open_ncdf(resource)
+NC_Read <- function(fileName, asTerra = TRUE) {
+  ds <- ncdfCF::open_ncdf(fileName)
+  if (asTerra) {
+    NCVars <- names(ds$variables()) # get variable names in netCDF
+    ds_ls <- lapply(NCVars, FUN = function(var) { # loop over variables and turn each into terra SpatRaster
+      ## ! "need to ensure that axis names are X or Y" otherwise, this fails (see example code above)
+      ds[[var]]$terra()
+    })
+    ds <- do.call(c, ds_ls) # combine all SpatRasters into a single SpatRaster
+    ## ! "need to assign correct variable name, long name, units, etc. to SpatRaster layers" - they seem lost in conversion
+  }
+  return(ds)
 }

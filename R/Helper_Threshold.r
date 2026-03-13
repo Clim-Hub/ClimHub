b014bbcf552773b32fd45b38b2ae7cc0074cb060
ErikKusch <erik@i-solution.de>
2026-03-13 11:03:56 +0100

    feat: Tresholding now also for ETCCDI QUantiles and arrays fix: thresholding logic now proper

--- R/Helper_Threshold.r
+++ R/Helper_Threshold.r
@@ -5,6 +5,7 @@
 #' @param CFVariable A CFVariable within which thresholding should be applied
 #' @param operator Character. One of ">", "<", ">=", "<=", "==", "!=" to evaluate CFVariable cells against threshold value.
 #' @param threshold Numeric or Character. threshold to evaluate CFVariable cells against using operator value.
+#' @param threshMode Character, optional. If supplying a CFVariable containing baseline quantiles for ETCCDI calculation, setting `threshMode = "ETCCDIQuantiles"` will internally apply location and day-of-year specific thresholds to `CFVariable` input. Default is `NULL` avoiding this behaviour.
 #' @param returnValues Logical. Whether to return CFVariable of values matching thresholding (TRUE) or only logical raster indicating where thresholding is matched (FALSE).
 #' @param returnSummary Function. Summary function for tresholded CFVariable.
 #' @param returnTResolution Character. Temporal resolution of summary of tresholded CFVariable.
@@ -27,25 +28,50 @@
 #'     returnValues = TRUE,
 #'     returnSummary = mean
 #' )
-Helper_Threshold <- function(CFVariable, operator, threshold, returnValues = FALSE, returnSummary = sum, returnTResolution = "year") {
+Helper_Threshold <- function(CFVariable, operator, threshold, threshMode = NULL, returnValues = FALSE, returnSummary, returnTResolution = "year") {
     ## input check, needs rwriting to Helper.InputChecker
     if (!operator %in% c(">", "<", ">=", "<=", "==", "!=")) {
         stop("operator must be one of: '>', '<', '>=', '<=', '==', '!='")
     }
 
-    ## apply thresholding
-    # Get the raw data array
+    ## Get the raw data array
     raw_array <- CFVariable$raw()
-    # array_dims <- dimnames(raw_array)
+    array_dims <- dimnames(raw_array)
 
-    # Apply the logical filter over the entire array at once
-    expr <- paste0("raw_array[raw_array", operator, threshold, "] <- NA")
-    eval(parse(text = expr))
+    ## reformatting treshold to array if necessary
+    if (class(threshold)[1] == "CFVariable") {
+        threshold <- threshold$raw()
+    }
 
+    ## reformatting of threshold if ETCCDI Baseline is specified
+    if (identical(threshMode, "ETCCDIQuantiles")) {
+        # Extract the time labels from dimnames
+        raw_dates <- as.Date(sub("T.*$", "", dimnames(raw_array)$time)) # drops the "T12:00:00"
+        thresh_dates <- as.Date(dimnames(threshold)$time)
+        # Create a month-day key for matching
+        raw_key <- format(raw_dates, "%m-%d")
+        thresh_key <- format(thresh_dates, "%m-%d")
+        # match dates
+        idx <- match(raw_key, thresh_key)
+        # Now expand `threshold` to a 3D array matching raw_array time length:
+        threshold <- threshold[, , idx, drop = FALSE]
+        dimnames(threshold) <- array_dims
+    }
+
+    ## apply thresholding
+    if (class(threshold) == "array") { # this is comparing to arrays and setting every exceeding value in the former to NA
+        expr <- paste("raw_array", operator, "threshold")
+    } else { # this is setting every value that does NOT match the operator threshold to NA
+        expr <- paste0("raw_array", operator, threshold)
+    }
+    mask <- eval(parse(text = expr))
+    raw_array[!mask] <- NA
+
+    ## binarise if return of values is not desired
     if (!returnValues) {
         raw_array <- (!is.na(raw_array)) * 1
     }
-    # dimnames(raw_array) <- array_dims
+    dimnames(raw_array) <- array_dims
 
     # Create a new CFVariable from the filtered data
     Thresh_CF <- as_CF("Thresholded", raw_array)
@@ -57,10 +83,14 @@ Helper_Threshold <- function(CFVariable, operator, threshold, returnValues = FAL
     apply(atts, 1, function(a) Thresh_CF$set_attribute(a$name, a$type, a$value))
 
     ## return summary
-    # when thresholding creates Inf values (e.g. dividing by the logical mask) we want to treat them as missing rather than drop them silently.
-    Thresh_CF$summarise(
-        "Tresholded",
-        returnSummary,
-        returnTResolution
-    )
+    if (exists("returnSummary")) {
+        Thresh_CF <- Thresh_CF$summarise(
+            "Tresholded",
+            returnSummary,
+            returnTResolution
+        )
+    }
+
+    ## return object
+    Thresh_CF
 }
